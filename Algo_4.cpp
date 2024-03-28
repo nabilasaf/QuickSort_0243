71dcb9cd71286be301a4dbbf4d0fbc83b72f3674
nabilasaf <nabilasafitri861@gmail.com>
2024-03-28 13:51:57 +0700

    Add project files.

---
+++ Algo_4.cpp
@@ -0,0 +1,10 @@
+// Algo_4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
